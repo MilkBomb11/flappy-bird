92ad2ac03edf9e5a3009816cb5cfd4f05a0ed7c1
MilkBomb11 <38152922+MilkBomb11@users.noreply.github.com>
2019-01-13 18:15:19 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-function Initialization()
+function Initialization() --This function resets pipes, bird, and score for another round
   objects = {}
   objects.bird = Bird(love.graphics.getWidth()/2, love.graphics.getHeight()/2, 15)
   objects.pipes = {}
@@ -6,7 +6,7 @@ function Initialization()
 end
 
 
-function love.load()
+function love.load() -- Called one at the start
   love.window.setMode(400, 600)
   love.window.setTitle("Flappy Bird")
 
@@ -29,32 +29,32 @@ function love.load()
   ----
 
   --variables--
-  pipeInterval = 150
-  pipeSpawnTime = 2
-  currentTime = 0
+  pipeInterval = 150 --space between pipes in a set of pipes
+  pipeSpawnTime = 2 --pipe spawn cool time
+  currentTime = 0 --current time to know if it's time to spawn another pipe
 
-  gameScene = "game"
+  gameScene = "game" -- variable that will handle the flow of the program
   ----
 end
 
-function love.update(dt)
-  if gameScene == "game" then
-    gameUpdate(dt)
+function love.update(dt) -- called every frame
+  if gameScene == "game" then --if scene is game
+    gameUpdate(dt) -- call function gameUpdate which is located in GameScenes/game.lua
+  end --else do nothing
+end
+
+function love.draw() --called every frame
+  if gameScene == "game" then --if scene is game
+    gameDisplay() -- call function gameDisplay which is located in GameScenes/game.lua
+  elseif gameScene == "gameOver" then --else if scene is game over 
+    gameoverDisplay() -- call function gameoverDisplay which is located in GameScenes/gameover.lua
   end
 end
 
-function love.draw()
-  if gameScene == "game" then
-    gameDisplay()
-  elseif gameScene == "gameOver" then
-    gameoverDisplay()
-  end
-end
-
-function love.keypressed(key, scancode, isrepeat)
-  if gameScene == "game" then
-    gameInput(key)
-  elseif gameScene == "gameOver" then
-    gameoverInput(key)
+function love.keypressed(key, scancode, isrepeat) -- called whenever a key is pressed
+  if gameScene == "game" then -- if scene is game
+    gameInput(key) -- call function gameInput which is located in GameScenes/game.lua
+  elseif gameScene == "gameOver" then -- else if scene is game over
+    gameoverInput(key) -- call function gameoverInput which is located in GameScenes/gameover.lua
   end
 end
